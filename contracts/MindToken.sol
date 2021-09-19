22d12f6fd5d18eacbf2a6ab0d7db672b7410a565
TrejGun <trejgun@gmail.com>
2021-09-19 13:33:57 +0800

    feat: add types with typechain test: add proper tests for MindToken

--- contracts/MindToken.sol
+++ contracts/MindToken.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MindToken is ERC20 {
+    address public owner;
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
         _mint(msg.sender, 100 * 10 ** uint(decimals()));
     }
 }

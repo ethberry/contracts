44e2fa12b9ce359d78ee8973e61458ce55a00f8c
TrejGun <trejgun@gmail.com>
2022-01-05 11:38:38 +0800

    docs: erc998

--- contracts/ERC998/interfaces/IERC998ERC721TopDown.sol
+++ contracts/ERC998/interfaces/IERC998ERC721TopDown.sol
@@ -4,35 +4,67 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
+/// @title ERC998ERC721 Top-Down Composable Non-Fungible Token
+/// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-998.md
+///  Note: the ERC-165 identifier for this interface is 0x1efdf36a
 interface IERC998ERC721TopDown is IERC721Receiver {
+
+  /// @dev This emits when a token receives a child token.
+  /// @param _from The prior owner of the token.
+  /// @param _tokenId The token that receives the child token.
+  /// @param _childContract The contract address of the child token.
+  /// @param _childTokenId The tokenId of the child.
   event ReceivedChild(
     address indexed _from,
     uint256 indexed _tokenId,
     address indexed _childContract,
     uint256 _childTokenId
   );
+
+  /// @dev This emits when a child token is transferred from a token to an address.
+  /// @param _tokenId The parent token that the child token is being transferred from.
+  /// @param _to The new owner address of the child token.
+  /// @param _childContract The contract address of the child token.
+  /// @param _childTokenId The tokenId of the child.
   event TransferChild(
-    uint256 indexed tokenId,
+    uint256 indexed _tokenId,
     address indexed _to,
     address indexed _childContract,
     uint256 _childTokenId
   );
 
+  /// @notice Get the root owner of tokenId.
+  /// @param _tokenId The token to query for a root owner address
+  /// @return rootOwner The root owner at the top of tree of tokens and ERC998 magic value.
   function rootOwnerOf(uint256 _tokenId)
   external
   view
   returns (bytes32 rootOwner);
 
+  /// @notice Get the root owner of a child token.
+  /// @param _childContract The contract address of the child token.
+  /// @param _childTokenId The tokenId of the child.
+  /// @return rootOwner The root owner at the top of tree of tokens and ERC998 magic value.
   function rootOwnerOfChild(address _childContract, uint256 _childTokenId)
   external
   view
   returns (bytes32 rootOwner);
 
+  /// @notice Get the parent tokenId of a child token.
+  /// @param _childContract The contract address of the child token.
+  /// @param _childTokenId The tokenId of the child.
+  /// @return parentTokenOwner The parent address of the parent token and ERC998 magic value
+  /// @return parentTokenId The parent tokenId of _tokenId
   function ownerOfChild(address _childContract, uint256 _childTokenId)
   external
   view
   returns (bytes32 parentTokenOwner, uint256 parentTokenId);
 
+  /// @notice Transfer child token from top-down composable to address.
+  /// @param _fromTokenId The owning token to transfer from.
+  /// @param _to The address that receives the child token
+  /// @param _childContract The ERC721 contract of the child token.
+  /// @param _childTokenId The tokenId of the token that is being transferred.
   function transferChild(
     uint256 _fromTokenId,
     address _to,
@@ -40,6 +72,11 @@ interface IERC998ERC721TopDown is IERC721Receiver {
     uint256 _childTokenId
   ) external;
 
+  /// @notice Transfer child token from top-down composable to address.
+  /// @param _fromTokenId The owning token to transfer from.
+  /// @param _to The address that receives the child token
+  /// @param _childContract The ERC721 contract of the child token.
+  /// @param _childTokenId The tokenId of the token that is being transferred.
   function safeTransferChild(
     uint256 _fromTokenId,
     address _to,
@@ -47,6 +84,12 @@ interface IERC998ERC721TopDown is IERC721Receiver {
     uint256 _childTokenId
   ) external;
 
+  /// @notice Transfer child token from top-down composable to address.
+  /// @param _fromTokenId The owning token to transfer from.
+  /// @param _to The address that receives the child token
+  /// @param _childContract The ERC721 contract of the child token.
+  /// @param _childTokenId The tokenId of the token that is being transferred.
+  /// @param _data Additional data with no specified format
   function safeTransferChild(
     uint256 _fromTokenId,
     address _to,
@@ -55,6 +98,13 @@ interface IERC998ERC721TopDown is IERC721Receiver {
     bytes memory _data
   ) external;
 
+  /// @notice Transfer bottom-up composable child token from top-down composable to other ERC721 token.
+  /// @param _fromTokenId The owning token to transfer from.
+  /// @param _toContract The ERC721 contract of the receiving token
+  /// @param _toTokenId The receiving token
+  /// @param _childContract The bottom-up composable contract of the child token.
+  /// @param _childTokenId The token that is being transferred.
+  /// @param _data Additional data with no specified format
   function transferChildToParent(
     uint256 _fromTokenId,
     address _toContract,
@@ -64,8 +114,11 @@ interface IERC998ERC721TopDown is IERC721Receiver {
     bytes memory _data
   ) external;
 
-  // getChild function enables older contracts like cryptokitties to be transferred into a composable
-  // The _childContract must approve this contract. Then getChild can be called.
+  /// @notice Get a child token from an ERC721 contract.
+  /// @param _from The address that owns the child token.
+  /// @param _tokenId The token that becomes the parent owner
+  /// @param _childContract The ERC721 contract of the child token
+  /// @param _childTokenId The tokenId of the child token
   function getChild(
     address _from,
     uint256 _tokenId,
--- contracts/ERC998/interfaces/IERC998ERC721TopDownEnumerable.sol
+++ contracts/ERC998/interfaces/IERC998ERC721TopDownEnumerable.sol
@@ -8,16 +8,29 @@ interface IERC998ERC721TopDownEnumerable {
   view
   returns (uint256);
 
+  // @notice Get child contract by tokenId and index
+  // @param _tokenId The parent token of child tokens in child contract
+  // @param _index The index position of the child contract
+  // @return childContract The contract found at the tokenId and index.
   function childContractByIndex(uint256 _tokenId, uint256 _index)
   external
   view
   returns (address childContract);
 
+  // @notice Get the total number of child tokens owned by tokenId that exist in a child contract.
+  // @param _tokenId The parent token of child tokens
+  // @param _childContract The child contract containing the child tokens
+  // @return uint256 The total number of child tokens found in child contract that are owned by tokenId.
   function totalChildTokens(uint256 _tokenId, address _childContract)
   external
   view
   returns (uint256);
 
+  // @notice Get child token owned by tokenId, in child contract, at index position
+  // @param _tokenId The parent token of the child token
+  // @param _childContract The child contract of the child token
+  // @param _index The index position of the child token.
+  // @return childTokenId The child tokenId for the parent token, child token and index
   function childTokenByIndex(
     uint256 _tokenId,
     address _childContract,

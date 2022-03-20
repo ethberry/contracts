337b55f3ba3e7982dc9fd3aa944863533a13c5a4
Yury Toporov <gemuniongroup@gmail.com>
2022-03-20 11:57:22 +0800

    refactor: add MaxChildType 998 preset

---
+++ contracts/ERC998/MaxChildType.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: PavelZavadskiy
+// Email: pavelzavadsky@gmail.com
+// GitHub: https://github.com/PavelZavadskiy
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract MaxChildType {
+
+  uint256 private _defaultMaxChildPerType = 1; // One Item per Hero's slot
+  mapping(uint256 /* ItemType */ => uint256 /* MaxChilds */) internal _maxChildTypes;
+  mapping(uint256 /* ItemType */ => uint256 /* NumChilds */) internal _numChildTypes;
+
+  event SetMaxChild(uint256 childType, uint256 maxCount);
+
+  function getMaxChild(uint256 childType) public view returns (uint256) {
+    if (_maxChildTypes[childType] > 1) {
+      return _maxChildTypes[childType];
+    }
+    return _defaultMaxChildPerType;
+  }
+
+  function _setMaxChild(uint256 childType, uint256 max) internal {
+    _maxChildTypes[childType] = max;
+    emit SetMaxChild(childType, max);
+  }
+
+  function incrementChildCount(uint256 childType) public {
+    uint256 max = _maxChildTypes[childType] > 0 ? _maxChildTypes[childType] : _defaultMaxChildPerType;
+    require(_numChildTypes[childType] < max, "MaxChildType: excess number of child");
+    _numChildTypes[childType]++;
+  }
+
+  function decrementChildCount(uint256 childType) public {
+    if (_numChildTypes[childType] > 0) {
+      _numChildTypes[childType]--;
+    }
+  }
+
+  modifier onlyChildTypeWithIncrement(uint256 childType) {
+    incrementChildCount(childType);
+    _;
+  }
+
+  modifier onlyChildTypeWithDecrement(uint256 childType) {
+    decrementChildCount(childType);
+    _;
+  }
+}
---
+++ contracts/ERC998/preset/ERC998ERC721TopDownMaxChildType.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun+gemunion@gmail.com
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.4;
+
+import "../ERC998ERC721TopDown.sol";
+import "../MaxChildType.sol";
+
+contract ERC998ERC721TopDownMaxChildType is ERC998ERC721TopDown, MaxChildType {
+
+  constructor(
+    string memory name,
+    string memory symbol,
+    string memory baseTokenURI,
+    uint256 cap
+  ) ERC998ERC721TopDown(name, symbol, baseTokenURI, cap) {}
+
+  function removeChild(
+    uint256 _tokenId,
+    address _childContract,
+    uint256 _childType,
+    uint256 _childTokenId
+  ) internal override onlyChildTypeWithDecrement(_childType) {
+    super.removeChild(_tokenId, _childContract, _childTokenId);
+  }
+
+  function receiveChild(
+    address _from,
+    uint256 _tokenId,
+    address _childContract,
+    uint256 _childType,
+    uint256 _childTokenId
+  ) internal override onlyChildTypeWithIncrement(_childType) {
+    super.receiveChild(_from, _tokenId, _childContract, _childTokenId);
+  }
+
+  function setMaxChild(uint256 childType, uint256 max) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    _setMaxChild(childType, max);
+  }
+}

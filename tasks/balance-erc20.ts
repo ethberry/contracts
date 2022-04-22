47f4e96032c362b1d2284542428bf199e9a9e855
TrejGun <trejgun@gmail.com>
2022-04-22 14:57:14 +0800

    chore: update tasks

--- tasks/balance-erc20.ts
+++ tasks/balance-erc20.ts
@@ -1,20 +1,14 @@
 import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-web3";
 
-task("balance-erc20", "Prints an ERC20's balance + ETH")
+task("balance-erc20", "Prints an ERC20 balance")
   .addParam("account", "The account's address")
   .addParam("contract", "The ERC20 contract's address")
   .setAction(async (args, hre) => {
-    const { account, contract } = args.account;
+    const { account, contract } = args;
 
-    const balance = await hre.web3.eth.getBalance(account);
-    console.info("ETH Balance:", hre.web3.utils.fromWei(balance, "ether"), "ETH");
-
-    const coinFactory = await hre.ethers.getContractFactory("TokenErc20");
+    const coinFactory = await hre.ethers.getContractFactory("ERC20ACBCS");
     const coinInstance = coinFactory.attach(contract);
     const accBalance = await coinInstance.balanceOf(account);
-    const accBalanceString = accBalance.toString();
 
-    console.info("ERC20 Balance in wei:", accBalanceString);
-    console.info("ERC20 Balance in ether:", hre.ethers.utils.formatEther(accBalanceString));
+    console.info("ERC20 Balance:", hre.ethers.utils.formatEther(accBalance.toString()));
   });
--- tasks/decode.ts
+++ tasks/decode.ts
@@ -4,18 +4,11 @@ import "@nomiclabs/hardhat-web3";
 task("decode", "Decode error message")
   .addParam("data", "encoded data")
   .setAction(async (args, hre) => {
+    const { data } = args;
+
+    // dummy
     await Promise.resolve();
 
-    function decodeMessage(code: string): string {
-      let codeString = `0x${code.substr(138)}`.replace(/0+$/, "");
-
-      // If the codeString is an odd number of characters, add a trailing 0
-      if (codeString.length % 2 === 1) {
-        codeString += "0";
-      }
-
-      return hre.ethers.utils.toUtf8String(codeString);
-    }
-
-    console.info(decodeMessage(args.data));
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.info(hre.ethers.utils.toUtf8String(`0x${data.substr(138)}`));
   });

62f79a8118ff503d9277e567e0af4399655a1fcf
TrejGun <trejgun@gmail.com>
2022-05-07 09:06:32 +0800

    feat: permit preset

---
+++ contracts/ERC20/preset/ERC20ACBP.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun+gemunion@gmail.com
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ERC20ACBP is AccessControl, ERC20Burnable, ERC20Permit {
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  constructor(
+    string memory name,
+    string memory symbol
+  ) ERC20(name, symbol) ERC20Permit(name) {
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    _setupRole(MINTER_ROLE, _msgSender());
+  }
+
+  function mint(address to, uint256 amount) public virtual onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl) returns (bool) {
+    return
+      interfaceId == type(IERC20).interfaceId ||
+      interfaceId == type(IERC20Metadata).interfaceId ||
+      super.supportsInterface(interfaceId);
+  }
+}

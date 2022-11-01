20f2688894faefb4bc0aeca1f829deb8cfcd9e72
Yury Toporov <gemuniongroup@gmail.com>
2022-11-01 22:43:37 +0800

    feat: publish all contracts in root, except ERC998

--- scripts/publish-abi.sh
+++ scripts/publish-abi.sh
@@ -4,20 +4,21 @@ set -e
 
 echo "### Release script started..."
 npm run build
-echo "### Build finished. Copying sols & abis."
-rm -rf abi
-mkdir -p abi
-# copy all abis to ./abi
-find artifacts/contracts ! -iregex ".*([a-zA-Z0-9_]).json" -exec cp {} abi \; 2>/dev/null
-# remove non-abi files
-rm abi/*.dbg.json
-# copy sol
-rm -rf sol
-mkdir -p sol
-cp -r contracts/** sol
+echo "### Build finished"
+echo "### Copying contract/** to root"
+#rm -rf abi
+#mkdir -p abi
+## copy all abis to ./abi
+#find artifacts/contracts ! -iregex ".*([a-zA-Z0-9_]).json" -exec cp {} abi \; 2>/dev/null
+## remove non-abi files
+#rm abi/*.dbg.json
+## copy sol
+#rm -rf sol
+#mkdir -p sol
+cp -r contracts/** ./
 # publish from root
 echo "### Publishing..."
 npm version patch -m "[Manual release] [skip ci] %s"
 npm publish
 git push --no-verify && git push --tags --no-verify
-echo "### Release Done."
+echo "### Release Done!"

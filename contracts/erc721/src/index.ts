f24d73a7436109311bd7548d71ba12619eb91998
TrejGun <trejgun@gmail.com>
2023-01-02 21:34:00 +0800

    test: export consecutive

--- contracts/erc721/src/index.ts
+++ contracts/erc721/src/index.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-web3";
 export * from "./base";
 export * from "./burnable/burn";
 export * from "./capped/capped";
+export * from "./consecutive/consecutive";
 export * from "./pausable/pausable";
 export * from "./royalty";
 export * from "./storage/storage";

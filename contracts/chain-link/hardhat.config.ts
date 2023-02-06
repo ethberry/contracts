c88eb9b05c4fab7489ca56bc39a4c94d439239a9
Yury Toporov <gemuniongroup@gmail.com>
2023-02-06 13:26:32 +0800

    feat: hh env [skip ci]

--- contracts/chain-link/hardhat.config.ts
+++ contracts/chain-link/hardhat.config.ts
@@ -15,39 +15,39 @@ export default {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
-      gas: "auto",
-      blockGasLimit: 40966424,
+      blockGasLimit: 1e10, // default: 3e7
+      gas: 7000000,
     },
     besu: {
       url: process.env.JSON_RPC_ADDR_BESU,
-      // https://besu.hyperledger.org/en/stable/Reference/Accounts-for-Testing/
+      timeout: 142000,
       accounts: [
         "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63", // 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73
         "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3", // 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
         "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f", // 0xf17f52151EbEF6C7334FAD080c5704D77216b732
       ],
     },
-    polygon: {
-      url: process.env.JSON_RPC_ADDR_POLYGON,
-    },
-    mumbai: {
-      url: process.env.JSON_RPC_ADDR_MUMBAI,
-      accounts: [process.env.MM_PRIVATEKEY],
-      gasPrice: 8000000000, // default is 'auto' which breaks chains without the london hardfork
-      timeout: 142000,
-      // gas: 29999915,
+    gemunion: {
+      url: process.env.JSON_RPC_ADDR_GEMUNION,
+      timeout: 30000,
+      accounts: [
+        "0x93551a60e21c15bd7cf36eb98cadba972e44aed3e7405f3bcee3c8d8fcb03c95", // 0x61284003E50b2D7cA2B95F93857abB78a1b0F3Ca
+        "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3", // 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
+        "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f", // 0xf17f52151EbEF6C7334FAD080c5704D77216b732
+      ],
     },
     binancetest: {
-      url: process.env.JSON_RPC_ADDR_BINANCE_TEST,
+      url: process.env.JSON_RPC_ADDR_BINANCE,
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: [process.env.MM_PRIVATEKEY],
+      accounts: {
+        mnemonic: process.env.MM_MNEMONIC,
+      },
     },
-    binancemain: {
-      url: process.env.JSON_RPC_ADDR_BINANCE,
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: [process.env.MM_PRIVATEKEY],
+    goerli: {
+      url: process.env.JSON_RPC_ADDR_GORLY,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      timeout: 142000,
     },
   },
   solidity: {

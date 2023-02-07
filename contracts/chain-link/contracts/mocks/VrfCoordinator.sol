fab3f8227815aac4c07e3460ebe52f9b1ecc3547
Yury Toporov <gemuniongroup@gmail.com>
2023-02-07 10:22:21 +0800

    feat: vrfCoordinator upd [skip ci]

--- contracts/chain-link/contracts/mocks/VrfCoordinator.sol
+++ contracts/chain-link/contracts/mocks/VrfCoordinator.sol
@@ -15,22 +15,24 @@ contract VRFCoordinatorMock is VRFRequestIDBase {
     LINK = LinkTokenInterface(linkAddress);
   }
 
-  mapping(bytes32 /* keyHash */ => uint256 /* nonce */) private nonces;
+  mapping(bytes32 /* provingKey */ => mapping(address /* consumer */ => uint256)) private nonces;
 
   function onTokenTransfer(address sender, uint256, bytes memory _data) public onlyLINK {
     (bytes32 keyHash, uint256 seed) = abi.decode(_data, (bytes32, uint256));
-    emit RandomnessRequest(sender, keyHash, seed);
 
-    uint256 vRFSeed = uint256(keccak256(abi.encode(keyHash, seed, address(sender), nonces[keyHash])));
+    emit RandomnessRequest(sender, keyHash, seed);
+    uint256 vRFSeed = uint256(keccak256(abi.encode(keyHash, seed, address(sender), nonces[keyHash][sender])));
     bytes32 _requestId = keccak256(abi.encodePacked(keyHash, vRFSeed));
+
     emit RandomnessRequestId(_requestId, sender);
+
+    nonces[keyHash][sender] = nonces[keyHash][sender] + 1;
+
+    // it doesn't work in the same transaction
     // callBackWithRandomness(_requestId, uint256(keccak256(abi.encode(123))), sender);
   }
-
-  VRFConsumerBase v;
-
   function callBackWithRandomness(bytes32 requestId, uint256 randomness, address consumerContract) public {
-    v = VRFConsumerBase(consumerContract);
+    VRFConsumerBase v = VRFConsumerBase(consumerContract);
     bytes memory resp = abi.encodeWithSelector(v.rawFulfillRandomness.selector, requestId, randomness);
     uint256 b = 206000;
     require(gasleft() >= b, "not enough gas for consumer");

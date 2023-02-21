07631f873f652219152feacad7593ec3af05541d
Yury Toporov <gemuniongroup@gmail.com>
2023-02-21 16:42:23 +0800

    feat: chainlink V2 BNB [skip ci]

---
+++ contracts/chain-link/contracts/extensions/ChainLinkBinanceTestnetV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun+gemunion@gmail.com
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.13;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+import "./ChainLinkBaseV2.sol";
+
+abstract contract ChainLinkBinanceTestnetV2 is ChainLinkBaseV2 {
+  constructor(
+    uint64 subId,
+    uint16 minReqConfs,
+    uint32 callbackGasLimit,
+    uint32 numWords
+  )
+  ChainLinkBaseV2(
+      address(0x6A2AAd07396B36Fe02a22b33cf443582f682c82f), // vrfCoordinatorV2 Binance testnet
+        0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314, // key hash 50 gwei
+      subId, minReqConfs, callbackGasLimit, numWords
+    )
+  {}
+}
+//LINK_ADDR=0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06
---
+++ contracts/chain-link/contracts/extensions/ChainLinkBinanceV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun+gemunion@gmail.com
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.13;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+import "./ChainLinkBaseV2.sol";
+
+abstract contract ChainLinkBinanceV2 is ChainLinkBaseV2 {
+  constructor(
+    uint64 subId,
+    uint16 minReqConfs,
+    uint32 callbackGasLimit,
+    uint32 numWords
+  )
+  ChainLinkBaseV2(
+      address(0xc587d9053cd1118f25F645F9E08BB98c9712A4EE), // vrfCoordinator BNB mainnet
+        0x17cd473250a9a479dc7f234c64332ed4bc8af9e8ded7556aa6e66d83da49f470, // key hash 1000 gwei
+      subId, minReqConfs, callbackGasLimit, numWords
+    )
+  {}
+}
+//LINK_ADDR=0x404460C6A5EdE2D891e8297795264fDe62ADBB75

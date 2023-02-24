988968e94d603bec68d687efd06331229ab5e745
TrejGun <trejgun@gmail.com>
2023-02-25 07:59:20 +0800

    fix: add supportsInterface

--- contracts/mocks/contracts/ERC20Receiver.sol
+++ contracts/mocks/contracts/ERC20Receiver.sol
@@ -6,6 +6,15 @@
 
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
 import "./extensions/ERC1363Receiver.sol";
 
-contract ERC20ReceiverMock is ERC1363Receiver {}
+contract ERC20ReceiverMock is ERC165, ERC1363Receiver {
+  function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+    return
+      interfaceId == type(IERC1363Receiver).interfaceId ||
+      interfaceId == type(IERC1363Spender).interfaceId ||
+      super.supportsInterface(interfaceId);
+  }
+}

8c04043ba0126a8144fbca5c86d10d539c5be9a5
TrejGun <trejgun@gmail.com>
2023-02-25 22:54:07 +0800

    fix: naming, part 4

--- contracts/erc721/contracts/mocks/import.sol
+++ contracts/erc721/contracts/mocks/import.sol
@@ -9,4 +9,4 @@ pragma solidity ^0.8.13;
 import "@gemunion/contracts-mocks/contracts/ERC721Receiver.sol";
 import "@gemunion/contracts-mocks/contracts/ERC721NonReceiver.sol";
 
-import "@gemunion/contracts-chain-link/contracts/mocks/VrfCoordinator.sol";
+import "@gemunion/contracts-chain-link/contracts/mocks/VRFCoordinator.sol";

eb17d904a37217654b148017fcb637e2431567ce
JenyaFomin <jenya.stasik@gmail.com>
2023-03-29 12:32:05 +0800

    feat(erc1363): add comments to ERC1363Receiver.sol

--- contracts/erc1363/contracts/extensions/ERC1363Receiver.sol
+++ contracts/erc1363/contracts/extensions/ERC1363Receiver.sol
@@ -6,10 +6,23 @@ pragma solidity ^0.8.13;
 import "../interfaces/IERC1363Receiver.sol";
 import "../interfaces/IERC1363Spender.sol";
 
+/**
+ * @dev Implementation of the {IERC1363Receiver} and {IERC1363Spender} interfaces.
+ * Allows to receive ERC1363 token transfers, and receive and approve them spending on behalf of the token owner.
+ */
 contract ERC1363Receiver is IERC1363Receiver, IERC1363Spender {
   event TransferReceived(address operator, address from, uint256 value, bytes data);
   event ApprovalReceived(address owner, uint256 value, bytes data);
 
+  /**
+   * @dev Called by a sender to notify the receiver of the transfer.
+   * @param operator The address of the operator performing the transfer.
+   * @param from The address of the sender.
+   * @param value The amount being transferred.
+   * @param data Additional data with no specified format.
+   * @return bytes4 selector of onTransferReceived function
+   *                unless throwing
+   */
   function onTransferReceived(
     address operator,
     address from,
@@ -26,6 +39,14 @@ contract ERC1363Receiver is IERC1363Receiver, IERC1363Spender {
     return this.onTransferReceived.selector;
   }
 
+  /**
+   * @dev Called by a sender to notify the receiver of the approval.
+   * @param owner The address of the owner providing the allowance.
+   * @param value The new allowance.
+   * @param data Additional data with no specified format.
+   * @return bytes4 selector of onApprovalReceived function
+   *                unless throwing
+   */
   function onApprovalReceived(address owner, uint256 value, bytes memory data) external override returns (bytes4) {
     if (data.length == 1) {
       if (data[0] == 0x00) return bytes4(0);

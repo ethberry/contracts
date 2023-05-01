4c8f84eed13c536c5213d5455d2f83ec03d331d3
TrejGun <trejgun@gmail.com>
2023-05-01 11:53:26 +0800

    feat: make support interface check for unsupported interfaces

--- helpers/mocha/src/supportInterface.ts
+++ helpers/mocha/src/supportInterface.ts
@@ -4,16 +4,29 @@ import { Contract } from "ethers";
 import { InterfaceId } from "@gemunion/contracts-constants";
 
 export function shouldSupportsInterface(factory: () => Promise<Contract>) {
-  return (...interfaces: Array<string>) => {
+  return (supportedInterfaces: Array<string>, unsupportedInterfaces: Array<string>) => {
     describe("supportsInterface", function () {
-      interfaces.forEach(iface => {
-        it(`Should support ${iface}`, async function () {
-          const contractInstance = await factory();
+      if (supportedInterfaces.length) {
+        supportedInterfaces.forEach(iface => {
+          it(`Should support ${iface}`, async function () {
+            const contractInstance = await factory();
 
-          const isSupported = await contractInstance.supportsInterface(iface);
-          expect(isSupported).to.equal(true);
+            const isSupported = await contractInstance.supportsInterface(iface);
+            expect(isSupported).to.equal(true);
+          });
         });
-      });
+      }
+
+      if (unsupportedInterfaces.length) {
+        unsupportedInterfaces.forEach(iface => {
+          it(`Should support ${iface}`, async function () {
+            const contractInstance = await factory();
+
+            const isSupported = await contractInstance.supportsInterface(iface);
+            expect(isSupported).to.equal(false);
+          });
+        });
+      }
 
       it(`Should not support ${InterfaceId.Invalid}`, async function () {
         const contractInstance = await factory();

b3ba2be242ca4253607270a9cabc271ce50ea053
TrejGun <trejgun@gmail.com>
2023-05-01 12:35:56 +0800

    feat: add new interfaves

--- helpers/constants/src/interface.ts
+++ helpers/constants/src/interface.ts
@@ -14,8 +14,10 @@ export enum InterfaceId {
   IERC721 = "0x80ac58cd",
   IERC721Metadata = "0x5b5e139f",
   IERC721Enumerable = "0x780e9d63",
+  IERC721Receiver = "0x150b7a02",
   IERC1155 = "0xd9b67a26",
   IERC1155Metadata = "0x0e89341c", // IERC1155MetadataURI
+  IERC1155Receiver = "0x4e2312e0",
   IRoyalty = "0x2a55205a", // IERC2981
   IAccessControl = "0x7965db0b",
   Invalid = "0xffffffff",

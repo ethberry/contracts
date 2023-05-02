53d126e16702c73e2914d265bb1e3e0077fd51b7
TrejGun <trejgun@gmail.com>
2023-05-02 11:34:50 +0800

    fix: remove project related ids

--- contracts/misc/contracts/constants.sol
+++ contracts/misc/contracts/constants.sol
@@ -22,7 +22,6 @@ bytes4 constant IERC1363_RECEIVER_ID = 0x88a7ca5c;
 bytes4 constant IERC1363_SPENDER_ID = 0x7b04a2d0;
 bytes4 constant IERC4906_ID = 0x49064906;
 bytes4 constant IERC4907_ID = 0xad092b5c;
-bytes4 constant IERC721_GRADE_ID = 0x381b3743;
-bytes4 constant IERC721_MYSTERY_ID = 0xf0f47261;
-bytes4 constant IERC721_RANDOM_ID = 0x32034d27;
-bytes4 constant IERC721_SIMPLE_ID = 0xdc662ff6;
+
+// WARNING: project related InterfaceIds like SIMPLE and RANDOM should not be here
+

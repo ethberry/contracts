57aabae8a460498d0ef692c50470f7a34a0d9fff
TrejGun <trejgun@gmail.com>
2023-08-15 18:18:55 +0800

    fix: remove traits

--- helpers/constants/src/attributes.ts
+++ helpers/constants/src/attributes.ts
@@ -4,4 +4,3 @@ export const TEMPLATE_ID = zeroPadValue(toUtf8Bytes("TEMPLATE_ID"), 32);
 export const RARITY = zeroPadValue(toUtf8Bytes("RARITY"), 32);
 export const LEVEL = zeroPadValue(toUtf8Bytes("LEVEL"), 32);
 export const GENES = zeroPadValue(toUtf8Bytes("GENES"), 32);
-export const TRAITS = zeroPadValue(toUtf8Bytes("TRAITS"), 32);

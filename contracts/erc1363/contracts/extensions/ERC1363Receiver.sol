529756f7a8eb35cfa2e4df2295d1bba80957d4d6
TrejGun <trejgun@gmail.com>
2023-10-15 13:28:12 +0800

    fix: restore ERC1363Receiver

---
+++ contracts/erc1363/contracts/extensions/ERC1363Receiver.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (interfaces/IERC1363.sol)
+
+pragma solidity ^0.8.20;
+
+import {IERC1363Receiver} from "../interfaces/IERC1363Receiver.sol";
+import {IERC1363Spender} from "../interfaces/IERC1363Spender.sol";
+
+/**
+ * @dev Implementation of the {IERC1363Receiver} and {IERC1363Spender} interfaces.
+ * Allows to receive ERC1363 token transfers, and receive and approve them spending on behalf of the token owner.
+ */
+contract ERC1363Receiver is IERC1363Receiver, IERC1363Spender {
+  event TransferReceived(address operator, address from, uint256 value, bytes data);
+  event ApprovalReceived(address owner, uint256 value, bytes data);
+
+  /**
+   * @dev Called by a sender to notify the receiver of the transfer.
+   * @param operator The address of the operator performing the transfer.
+   * @param from The address of the sender.
+   * @param value The amount being transferred.
+   * @param data Additional data with no specified format.
+   * @return bytes4 selector of onTransferReceived function
+   *                unless throwing
+   */
+  function onTransferReceived(
+    address operator,
+    address from,
+    uint256 value,
+    bytes memory data
+  ) external override returns (bytes4) {
+    if (data.length == 1) {
+      if (data[0] == 0x00) return bytes4(0);
+      if (data[0] == 0x01) revert("onTransferReceived revert");
+      if (data[0] == 0x02) revert();
+      if (data[0] == 0x03) assert(false);
+    }
+    emit TransferReceived(operator, from, value, data);
+    return this.onTransferReceived.selector;
+  }
+
+  /**
+   * @dev Called by a sender to notify the receiver of the approval.
+   * @param owner The address of the owner providing the allowance.
+   * @param value The new allowance.
+   * @param data Additional data with no specified format.
+   * @return bytes4 selector of onApprovalReceived function
+   *                unless throwing
+   */
+  function onApprovalReceived(address owner, uint256 value, bytes memory data) external override returns (bytes4) {
+    if (data.length == 1) {
+      if (data[0] == 0x00) return bytes4(0);
+      if (data[0] == 0x01) revert("onApprovalReceived revert");
+      if (data[0] == 0x02) revert();
+      if (data[0] == 0x03) assert(false);
+    }
+    emit ApprovalReceived(owner, value, data);
+    return this.onApprovalReceived.selector;
+  }
+}
--- contracts/mocks/contracts/Wallet.sol
+++ contracts/mocks/contracts/Wallet.sol
@@ -10,12 +10,12 @@ import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {ERC721Holder, IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {ERC1155Holder, IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-import {ERC1363Holder, IERC1363Receiver, IERC1363Spender} from "@gemunion/contracts-erc1363/contracts/extensions/ERC1363Holder.sol";
+import {ERC1363Receiver, IERC1363Receiver, IERC1363Spender} from "@gemunion/contracts-erc1363/contracts/extensions/ERC1363Holder.sol";
 
 /**
  *@dev Wallet contract can receive ETH, ERC1363 tokens, ERC721 tokens, and ERC1155 tokens.
  */
-contract Wallet is ERC165, ERC721Holder, ERC1155Holder, ERC1363Holder {
+contract Wallet is ERC165, ERC721Holder, ERC1155Holder, ERC1363Receiver {
   receive() external payable virtual {}
 
   /**

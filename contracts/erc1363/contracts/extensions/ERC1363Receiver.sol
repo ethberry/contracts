292bfb654b9c05384f78ee6ba65f614807d505e8
TrejGun <trejgun@gmail.com>
2023-10-16 13:40:15 +0800

    fix: remove testing bindings

--- contracts/erc1363/contracts/extensions/ERC1363Receiver.sol
+++ contracts/erc1363/contracts/extensions/ERC1363Receiver.sol
@@ -29,12 +29,6 @@ contract ERC1363Receiver is IERC1363Receiver, IERC1363Spender {
     uint256 value,
     bytes memory data
   ) external override returns (bytes4) {
-    if (data.length == 1) {
-      if (data[0] == 0x00) return bytes4(0);
-      if (data[0] == 0x01) revert("onTransferReceived revert");
-      if (data[0] == 0x02) revert();
-      if (data[0] == 0x03) assert(false);
-    }
     emit TransferReceived(operator, from, value, data);
     return this.onTransferReceived.selector;
   }
@@ -48,12 +42,6 @@ contract ERC1363Receiver is IERC1363Receiver, IERC1363Spender {
    *                unless throwing
    */
   function onApprovalReceived(address owner, uint256 value, bytes memory data) external override returns (bytes4) {
-    if (data.length == 1) {
-      if (data[0] == 0x00) return bytes4(0);
-      if (data[0] == 0x01) revert("onApprovalReceived revert");
-      if (data[0] == 0x02) revert();
-      if (data[0] == 0x03) assert(false);
-    }
     emit ApprovalReceived(owner, value, data);
     return this.onApprovalReceived.selector;
   }

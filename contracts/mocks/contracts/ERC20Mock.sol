ec81523f2cb5342d29e95eb8d26889d6c2bf4e78
TrejGun <trejgun@gmail.com>
2023-10-22 11:06:28 +0800

    test: erc20 mock

--- contracts/mocks/contracts/ERC20Mock.sol
+++ contracts/mocks/contracts/ERC20Mock.sol
@@ -6,27 +6,18 @@
 
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 import {DEFAULT_ADMIN_ROLE, MINTER_ROLE} from "@gemunion/contracts-utils/contracts/roles.sol";
 
-contract ERC20Mock is AccessControl, ERC20, ERC20Capped {
-  constructor(string memory name, string memory symbol, uint256 cap) ERC20(name, symbol) ERC20Capped(cap) {
+contract ERC20Mock is AccessControl, ERC20Burnable {
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _grantRole(MINTER_ROLE, _msgSender());
   }
 
-  function mint(address to, uint256 amount) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+  function mint(address to, uint256 amount) public virtual onlyRole(MINTER_ROLE) {
     _mint(to, amount);
-  }
-
-  function _update(
-    address from,
-    address to,
-    uint256 amount
-  ) internal virtual override (ERC20, ERC20Capped) {
-    super._update(from, to, amount);
   }
 }

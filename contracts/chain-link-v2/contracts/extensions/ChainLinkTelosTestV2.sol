6efc9174323d72eda7c99a55823508568b0ac633
Yury Toporov <gemuniongroup@gmail.com>
2024-06-07 11:18:24 +0800

    feat: telos testnet chainlink

---
+++ contracts/chain-link-v2/contracts/extensions/ChainLinkTelosTestV2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun@gemunion.io
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.20;
+
+import { ChainLinkBaseV2 } from "./ChainLinkBaseV2.sol";
+
+abstract contract ChainLinkTelosTestV2 is ChainLinkBaseV2 {
+  constructor(
+    uint64 subId,
+    uint16 minReqConfs,
+    uint32 callbackGasLimit,
+    uint32 numWords
+  )
+    ChainLinkBaseV2(
+      address(0x33040c29f57f126b90d9528a5ee659d7a604b835), // vrfCoordinatorV2 Telos testnet
+      0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314, // key hash 50 gwei
+      subId,
+      minReqConfs,
+      callbackGasLimit,
+      numWords
+    )
+  {}
+}
+//LINK_ADDR=0x6eab1c5259173c4fea5667b4aad6529f4fc3176e

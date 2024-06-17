bb6daacec40396fc120669ec5ac84db2e4a30cb6
TrejGun <trejgun@gmail.com>
2024-06-17 13:12:03 +0800

    fix: tests runner

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,12 +13,15 @@ lerna exec --scope @gemunion/contracts-chain-link -- npm run test
 lerna exec --scope @gemunion/contracts-chain-link-v2 -- npm run test
 lerna exec --scope @gemunion/contracts-erc20 -- npm run test
 lerna exec --scope @gemunion/contracts-erc721 -- npm run test
-lerna exec --scope @gemunion/contracts-erc721e -- npm run test
 lerna exec --scope @gemunion/contracts-erc721c -- npm run test
+lerna exec --scope @gemunion/contracts-erc721e -- npm run test
 lerna exec --scope @gemunion/contracts-erc1155 -- npm run test
+lerna exec --scope @gemunion/contracts-erc1363 -- npm run test
 lerna exec --scope @gemunion/contracts-mocks -- npm run test
-lerna exec --scope @gemunion/contracts-polygon -- npm run test
+lerna exec --scope @gemunion/contracts-utils -- npm run test
+
 lerna exec --scope @gemunion/contracts-constants -- npm run test
-lerna exec --scope @gemunion/contracts-utils -- npm run test
-lerna exec --scope @gemunion/contracts-utils -- npm run test
+lerna exec --scope @gemunion/contracts-helpers -- npm run test
+lerna exec --scope @gemunion/traits-v5 -- npm run test
+lerna exec --scope @gemunion/traits-v6 -- npm run test
 

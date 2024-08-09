c6def5114a98fbd70e5cf66a51006e534f2ecac1
Yury Toporov <gemuniongroup@gmail.com>
2024-08-09 13:58:07 +0800

    fix: vrfv2plus gemunion [skip ci]

--- contracts/chain-link-v2-plus/contracts/extensions/ChainLinkGemunionBesuV2Plus.sol
+++ contracts/chain-link-v2-plus/contracts/extensions/ChainLinkGemunionBesuV2Plus.sol
@@ -15,7 +15,7 @@ abstract contract ChainLinkGemunionBesuV2Plus is ChainLinkBaseV2Plus {
     uint32 numWords
   )
     ChainLinkBaseV2Plus(
-      address(0xa50a51c09a5c451C52BB714527E1974b686D8e77), // besu vrfCoordinatorV2
+      address(0xd76746ff27eee2bbbe37228571b0b322d73976ed), // besu gemunion vrfCoordinatorV2Plus
       0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186, // keyHash
       minReqConfs,
       callbackGasLimit,

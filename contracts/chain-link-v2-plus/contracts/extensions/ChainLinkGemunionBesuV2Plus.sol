b2140b597cb8b11a762e52978352c02fc636218e
TrejGun <trejgun@gmail.com>
2024-09-01 09:10:21 +0800

    fix: address checksum

--- contracts/chain-link-v2-plus/contracts/extensions/ChainLinkGemunionBesuV2Plus.sol
+++ contracts/chain-link-v2-plus/contracts/extensions/ChainLinkGemunionBesuV2Plus.sol
@@ -15,7 +15,7 @@ abstract contract ChainLinkGemunionBesuV2Plus is ChainLinkBaseV2Plus {
     uint32 numWords
   )
     ChainLinkBaseV2Plus(
-      address(0x86c86939c631d53c6d812625bd6ccd5bf5beb774), // besu gemunion vrfCoordinatorV2Plus
+      address(0x86C86939c631D53c6D812625bD6Ccd5Bf5BEb774), // besu gemunion vrfCoordinatorV2Plus
       0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186, // keyHash
       minReqConfs,
       callbackGasLimit,
